05c95fa2b33317292282b0fba234c3ba78cf5f63
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-03 12:18:26 -0500

    pushing at video 11 with an error

---
+++ learn-redux/client/store.js
@@ -0,0 +1,23 @@
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+//import the rot reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+
+// create an object for he default data
+
+const defaultState = {
+    posts, 
+    comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
